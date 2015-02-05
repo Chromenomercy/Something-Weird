ea72f41acc93a72ed7767546be136ca0e3e465c1
Chromadude <PeterSearby8@googlemail.com>
2015-02-05 21:04:48 +0000

    creating fizzbuzz

--- Something Strange/Program.cs
+++ Something Strange/Program.cs
@@ -10,7 +10,20 @@ namespace Something_Strange
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to FizzBuzz!\n");
+            for (int i = 1; i <= 100; i++)
+            {
+                bool multiple_of_3 = i % 3 == 0;
+                bool multiple_of_5 = i % 5 == 0;
+                if (multiple_of_3 && multiple_of_5)
+                    Console.WriteLine("FizzBuzz");
+                else if (multiple_of_3)
+                    Console.WriteLine("Fizz");
+                else if (multiple_of_5)
+                    Console.WriteLine("Buzz");
+                else
+                    Console.WriteLine(i);
+            }
             Console.ReadKey();
         }
     }
