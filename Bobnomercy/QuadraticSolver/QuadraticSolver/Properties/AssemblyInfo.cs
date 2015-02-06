5fb8f48fe2772e2cfc378661d720f76e8ee9358b
Bobnomercy <jack.parsons.uk@gmail.com>
2015-02-06 18:05:33 +0000

    odfsdjf

--- Bobnomercy/QuadraticSolver/QuadraticSolver/Properties/AssemblyInfo.cs
+++ Bobnomercy/QuadraticSolver/QuadraticSolver/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6b846a8d-9987-424b-8e50-592b8a20c7c7")]
+[assembly: Guid("f721d0e7-33ab-44b6-9f70-d22d94f7b360")]
 
 // Version information for an assembly consists of the following four values:
 //
