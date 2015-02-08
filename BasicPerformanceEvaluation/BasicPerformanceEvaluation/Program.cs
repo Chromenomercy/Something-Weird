51ae6075b4455c67eb8f256918baf8dd7671bcef
Bobnomercy <jack.parsons.uk@gmail.com>
2015-02-08 12:24:39 +0000

    added ms

--- BasicPerformanceEvaluation/BasicPerformanceEvaluation/Program.cs
+++ BasicPerformanceEvaluation/BasicPerformanceEvaluation/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BasicPerformanceEvaluation
 {
@@ -11,29 +12,32 @@ namespace BasicPerformanceEvaluation
     {
         public static void Main(string[] args)
         {
+            Console.Write("\nEnter Name: ");
+            string name = Console.ReadLine();
+
             Console.Write("\nPress Enter to begin performance evaluation");
             Console.ReadLine();
 
-            SpeedTest();
+            SpeedTest(name);
 
             Console.Write("\nPress Enter to exit performance evaluation");
             Console.ReadLine();
         }
 
 
-        private static void SpeedTest()
+        private static void SpeedTest(string name)
         {
             Stopwatch stop_watch = new Stopwatch();
-            long num_iterations = 100000000;
-            int num_tests = 30;
+            long num_iterations = 500000000;
+            int num_tests = 10;
             long total_result = 0;
-            long out1 = 3 * 10 ^ 100000;
-            long out2 = 3 * 10 ^ 100000;
+            long out1 = 3 * 10 ^ 10000000000000;
+            long out2 = 3 * 10 ^ 10000000000000;
             
 
             for (int test = 0; test < num_tests; test++) {
 
-                Console.WriteLine("(running test " + (test + 1) + ")");
+                Console.Write("(running test " + (test + 1) + ") ");
 
                 stop_watch.Start();
 
@@ -43,11 +47,14 @@ namespace BasicPerformanceEvaluation
                     out2 -= out2;
                 }
 
+                Console.WriteLine(stop_watch.ElapsedMilliseconds + " ms");
                 total_result += Convert.ToInt64(stop_watch.ElapsedMilliseconds);
                 stop_watch.Reset();
             }
 
             long average_ms = total_result / num_tests;
+
+            //StreamWriter record = new StreamWriter("TestScores.txt");
 
             Console.WriteLine("\nSpeed test result = " + average_ms);
 
