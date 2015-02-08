fdb85e401792819d56203b3454ab256535741af4
Bobnomercy <jack.parsons.uk@gmail.com>
2015-02-08 13:44:47 +0000

    Changes including writing and reading to files, and also loops

--- BasicPerformanceEvaluation/BasicPerformanceEvaluation/Program.cs
+++ BasicPerformanceEvaluation/BasicPerformanceEvaluation/Program.cs
@@ -15,15 +15,36 @@ namespace BasicPerformanceEvaluation
             Console.Write("\nEnter Name: ");
             string name = Console.ReadLine();
 
-            Console.Write("\nPress Enter to begin performance evaluation");
-            Console.ReadLine();
+            Console.Write("\nEnter how many times to run evaluation, or -1 for an infinate loop: ");
+            string setting = Console.ReadLine();
+            if (setting == "-1"){
 
-            SpeedTest(name);
+                while (true)
+                {
+                    SpeedTest(name);
+                }
+            }
+
+            else{
+                try
+                {
+                    int num_full_tests = Convert.ToInt32(setting);
+                    while (num_full_tests > 0)
+                    {
+                        SpeedTest(name);
+                        num_full_tests -= 1;
+                    }
+                }
+                catch
+                {
+
+                    Console.Write("Invalid command");
+                }
+            }
 
             Console.Write("\nPress Enter to exit performance evaluation");
             Console.ReadLine();
         }
-
 
         private static void SpeedTest(string name)
         {
@@ -31,10 +52,10 @@ namespace BasicPerformanceEvaluation
             long num_iterations = 500000000;
             int num_tests = 10;
             long total_result = 0;
-            long out1 = 3 * 10 ^ 10000000000000;
-            long out2 = 3 * 10 ^ 10000000000000;
+            long out1 = 10000000;
+            long out2 = 10000000;
             
-
+            //test
             for (int test = 0; test < num_tests; test++) {
 
                 Console.Write("(running test " + (test + 1) + ") ");
@@ -47,16 +68,38 @@ namespace BasicPerformanceEvaluation
                     out2 -= out2;
                 }
 
-                Console.WriteLine(stop_watch.ElapsedMilliseconds + " ms");
                 total_result += Convert.ToInt64(stop_watch.ElapsedMilliseconds);
+                Console.WriteLine(stop_watch.ElapsedMilliseconds + " ms");
                 stop_watch.Reset();
             }
 
             long average_ms = total_result / num_tests;
 
-            //StreamWriter record = new StreamWriter("TestScores.txt");
+            StringBuilder newtext = new StringBuilder();
 
-            Console.WriteLine("\nSpeed test result = " + average_ms);
+            //write score and name to text doc
+            try
+            {
+                using (StreamReader text = new StreamReader("TestScores.txt"))
+                {
+                    newtext.AppendLine(text.ReadToEnd());
+                    newtext.AppendLine(Convert.ToString(name + ": " + average_ms));
+                }
+                Console.WriteLine("Found TextScores.txt");
+            }
+
+            catch
+            {
+                Console.WriteLine("Creating new file names TestScores.txt");
+                newtext.AppendLine(Convert.ToString(name + ": " + average_ms));
+            }
+
+            StreamWriter out_file = new StreamWriter("TestScores.txt");
+
+            out_file.Write(newtext.ToString());
+            out_file.Close();
+
+            Console.WriteLine("\nSpeed test result = " + average_ms + "\n");
 
         }
     }
